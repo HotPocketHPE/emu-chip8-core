7cd757a2971658746b924930eac79d4c6db793f1
HotPocketHPE <hotpockethpe@gmail.com>
2022-07-12 12:19:05 -0500

    Moved to new workspace

--- src/ui/display_ui.rs
+++ src/ui/display_ui.rs
@@ -1,10 +1,20 @@
-use egui::Context;
+use sdl2::{render::TextureCreator, video::WindowContext, surface::Surface};
 
 
-pub fn draw_display(ctx: &Context) {
-    egui::CentralPanel::default().show(ctx, |ui| {
-        let grid_width = 64;
-        let grid_height = 32;
-        
-    });
+
+pub fn build_display_texture(tc: TextureCreator<WindowContext>) {
+    
+}
+
+fn build_pixel_data<'a>(data_bools: &[&[bool]], buffer: &'a mut [u8]) -> Surface<'a> {
+    let width = data_bools.len();
+    let height = data_bools[0].len();
+    assert!(buffer.len() == width * height);
+    for i in 0..width {
+        for j in 0..height {
+            buffer[i * width + j] = (if data_bools[i][j] {0b10000000} else {0});
+        }
+    }
+    return Surface::from_data(buffer, data_bools.len() as u32, data_bools[0].len() as u32, 8, 
+        sdl2::pixels::PixelFormatEnum::Index1MSB).unwrap();
 }
--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,9 +1,12 @@
 mod display_ui;
 pub mod update_keypress;
 
-use eframe::egui;
+use std::time::Duration;
 
-use self::display_ui::draw_display;
+use eframe::egui;
+use sdl2::{pixels::Color, event::Event, keyboard::Keycode};
+
+use self::display_ui::{build_display_texture};
 
 
 #[derive(Default)]
@@ -26,6 +29,46 @@ impl eframe::App for EguiApp {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("CHIP-8 Emulator");
         });
-        draw_display(ctx);
     }
- }
+}
+
+pub fn run_sdl() {
+    let width = 640;
+    let height = 480;
+
+    let sdl_context = sdl2::init().unwrap();
+    let sdl_video_subsystem = sdl_context.video().unwrap();
+
+    let window = sdl_video_subsystem.window("CHIP-8 Emulator", width, height)
+        .position_centered()
+        .resizable()
+        .build()
+        .unwrap();
+
+        let mut canvas = window.into_canvas().build().unwrap();
+ 
+        canvas.set_draw_color(Color::RGB(0, 255, 255));
+        build_display_texture(canvas.texture_creator());
+        canvas.clear();
+        canvas.present();
+        let mut event_pump = sdl_context.event_pump().unwrap();
+        let mut i = 0;
+        'running: loop {
+            i = (i + 1) % 255;
+            canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
+            canvas.clear();
+            for event in event_pump.poll_iter() {
+                match event {
+                    Event::Quit {..} |
+                    Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                        break 'running
+                    },
+                    _ => {}
+                }
+            }
+            // The rest of the game loop goes here...
+    
+            canvas.present();
+            ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+        }
+    }
